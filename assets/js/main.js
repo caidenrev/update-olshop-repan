eb31000bbe81f4b7245d142b707fc5923373b725
caidenrev <cimicimicilung4868@@gmail.com>
2024-11-22 16:36:22 +0700

    olshop update

---
+++ assets/js/main.js
@@ -0,0 +1,128 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== SHOW CART ===============*/
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart__close");
+
+/*===== CART SHOW =====*/
+/* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
+
+/*===== CART HIDDEN =====*/
+/* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
+
+/*=============== SHOW LOGIN ===============*/
+const login = document.getElementById("login"),
+  loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close");
+
+/*===== LOGIN SHOW =====*/
+/* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.add("show-login");
+  });
+}
+
+/*===== LOGIN HIDDEN =====*/
+/* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
+
+/*=============== HOME SWIPER ===============*/
+var homeSwiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*=============== GANTI/TRANSFORM BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("scroll-header")
+    : header.classList.remove("scroll-header");
+};
+window.addEventListener("scroll", scrollHeader);
+scrollHeader();
+
+// TRANSFORM TEXT NAVBAR JADI PUTIH IF SCROLL Y ( >= 50 ) OF THE VIEWPORT !
+
+/*=============== NEW SWIPER ===============*/
+var newSwiper = new Swiper(".new-swiper", {
+  spaceBetween: 16,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: "true",
+});
+
+/*=============== SHOW SCROLL UP ===============*/
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+scrollUp();
+
+/*=============== LIGHT BOX ===============*/
+
+/*=============== QUESTIONS ACCORDION ===============*/
+
+/*=============== STYLE SWITCHER ===============*/
+
+// *============= SCROLL ACTIVE HOVER ==============*
+
+window.addEventListener("scroll", function () {
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  if (window.scrollY >= 50) {
+    navLinks.forEach((link) => {
+      link.classList.add("scroll-active");
+    });
+  } else {
+    navLinks.forEach((link) => {
+      link.classList.remove("scroll-active");
+    });
+  }
+});
